7590f097c811a9a05b043246490b07b031c3beff
Felixdev1996 <felixchizoba01@gmail.com>
2024-12-12 19:06:06 +0100

    newone

--- client/src/LoanprogramComponents/Loan.jsx
+++ client/src/LoanprogramComponents/Loan.jsx
@@ -1,29 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Loan2 from './Loan2';
+
+
+const imageUrl = "https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/amecreho010.png";
 
 function Loan() {
   return (
 
     <div>
       
-      {/* <div className='bg-red-500 h-[280px] md:h-[280px] lg:h-[250px] mt-[50px] md:mt-[45px] grid grid-cols-1   md:grid-cols-[38%,62%] lg:grid-cols-2'>
-        <div className='bg-gray-500 h-fit md:h-full'>
-          <p className='text-[rgb(12,51,121)] tracking-wider text-[17px] md:text-[18px] lg:text-[19px] ml-[10px] lg:ml-[80px] md:mt-[120px]'>PAGE LOAN PROGRAM</p>
-        </div>
-        
-        <div className='h-full bg-green-500'>
-
-        </div>
-      </div> */}
-
-
       <div>
-      <main className='pg grid justify-center items-center h-[250px] mt-10 mb-1'>
+        <main className='pg grid justify-center items-center h-[250px] mt-10 mb-1'>
           <h2 className='text-xl font-bold text-white md:text-2xl lg:text-4xl pt-7'>Loan Program Page</h2>
           <div className='flex items-center justify-center gap-1'>
           <h2 className='text-blue-400'>Home /</h2><h2 className='text-white'> Loan Program</h2>
-       x</div>
-      </main>
-    </div>
+            x
+          </div>
+        </main>
+      </div>
+
+
+      <div className='bg-white h-[2050px] md:h-[710px] lg:h-[700px] relative bottom-[90px] md:bottom-[60px] lg:bottom-[30px] grid grid-cols-1 md:grid-cols-[35%,65%] lg:grid-cols-[35%,65%] mt-[200px]'>
+        <div className='h-[570px] md:h-[709px] lg:h-[700px] bg-white'>
+          <img src={imageUrl} className='h-650px md:h-[550px] lg:h-[700px] left-[-100px] lg:left-[-50px] md:left-[-120px] absolute'/>
+        </div>
+        <div className='bg-white grid grid-cols-1 md:grid-cols-2 gap-[30px] md:gap-[15px] pr-[40px] md:pr-[50px] lg:pr-[100px] pl-[15px] md:pl-0 h-[1500px] md:h-[710px] lg:h-[700px]'>
+
+          <div className='bg-green-500 h-fit rounded-[10px] overflow-hidden hover:bg-[rgb(38,106,225)] mb-0'>
+            <div className='grid place-items-center bg-gradient-to-b from-[rgb(64,129,255)] to-[rgb(126,178,255)] h-fit pb-[40px] transform hover:scale-110 transition-all duration-[1.5s] hover:bg-[rgb(38,106,225)] man'>
+              <p className='w-[250px] md:w-[140px] lg:w-[250px] text-[rgb(21,63,139)] text-[20px] font-bold mt-[50px] tracking-wider leading-[25px] mr-[20px] lg:mr-[80px] hover:text-white transition-all duration-[1.5s] manboy'>Want a quote from this buisiness?</p>
+
+              <button className='bg-[rgb(12,51,121)] text-white py-[10px] px-[25px] whitespace-normal mt-[20px] mr-[150px] md:mr-[40px] lg:mr-[200px] rounded-[10px]'>Get a Quote</button>
+            </div>
+          </div>
+
+
+          <div className='h-fit bg-white rounded-[10px] border-[rgb(214,227,249)] border-solid border-[1px] hover:bg-gradient-to-b
+           hover:from-[rgb(155,203,249)] hover:to-[rgb(239,246,255)] hover:border-b-[rgb(41,108,225)] hover:border-b-[2px] transition duration-[0.5s] mb-0'>
+            
+              <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc09.png" className='w-[90px] md:w-[60px] lg:w-[70px] mt-[20px] ml-[25px] transform hover:scale-75 transition-all duration-[0.5s]' />
+
+              <p className='text-[rgb(38,106,225)] text-[20px] mt-[20px] ml-[25px]'>Personal Loan</p>
+
+              <p className='text-[rgb(186,141,139)] mt-[20px] ml-[25px] mb-[25px]'>Quick and easy application process</p>
+            
+          </div>
+
+
+          <div className='h-fit  bg-white rounded-[10px] border-[rgb(214,227,249)] border-solid border-[1px] hover:bg-gradient-to-b
+           hover:from-[rgb(155,203,249)] hover:to-[rgb(239,246,255)] hover:border-b-[rgb(41,108,225)] hover:border-b-[2px] transition duration-[0.5s] mb-0'>
+
+            <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc012.png"  className='w-[90px] md:w-[60px] lg:w-[70px] mt-[20px] ml-[25px] transform hover:scale-75 transition-all duration-[0.5s]'/>
+
+            <p className='text-[rgb(38,106,225)] text-[20px] mt-[20px] ml-[25px]'>Business Capital</p>
+
+            <p className='text-[rgb(186,141,139)] mt-[20px] ml-[25px] mb-[25px]'>Competitive interest rates</p>
+          </div>
+
+
+          <div className='h-fit bg-white rounded-[10px] border-[rgb(214,227,249)] border-solid border-[1px] hover:bg-gradient-to-b
+           hover:from-[rgb(155,203,249)] hover:to-[rgb(239,246,255)] hover:border-b-[rgb(41,108,225)] hover:border-b-[2px] transition duration-[0.5s] mb-0'>
+
+            <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc011.png"  className='w-[90px] md:w-[60px] lg:w-[70px] mt-[20px] ml-[25px] transform hover:scale-75 transition-all duration-[0.5s]'/>
+
+            <p className='text-[rgb(38,106,225)] text-[20px] mt-[20px] ml-[25px]'>Business Loan</p>
+            
+
+            <p className='text-[rgb(186,141,139)] mt-[20px] ml-[25px] mb-[25px]'>Flexible repayment terms</p>
+
+           </div>
+
+
+          <div className='h-fit bg-white rounded-[10px] border-[rgb(214,227,249)] border-solid border-[1px] hover:bg-gradient-to-b
+           hover:from-[rgb(155,203,249)] hover:to-[rgb(239,246,255)] hover:border-b-[rgb(41,108,225)] hover:border-b-[2px] transition duration-[0.5s] mb-0'>
+
+            <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc010.png"  className='w-[90px] md:w-[60px] lg:w-[70px] mt-[20px] ml-[25px] transform hover:scale-75 transition-all duration-[0.5s]'/>
+
+            <p className='text-[rgb(38,106,225)] text-[20px] mt-[20px] ml-[25px]'>Investica Network</p>
+
+            <p className='text-[rgb(186,141,139)] mt-[20px] ml-[25px] mb-[25px]'>Networking with like-minded professionals</p>
+
+           </div>
+
+
+          <div className='bg-green-500 h-fit rounded-[10px] overflow-hidden hover:bg-[rgb(38,106,225)] mb-0'>
+
+            <div className='grid place-items-center bg-gradient-to-b from-[rgb(64,129,255)] to-[rgb(126,178,255)] h-fit pb-[40px] transform hover:scale-110 transition-all duration-[1.5s] hover:bg-[rgb(38,106,225)] man'>
+              <p className='w-[250px] md:w-[140px] lg:w-[250px] text-[rgb(21,63,139)] text-[20px] font-bold mt-[50px] tracking-wider leading-[25px] mr-[20px] lg:mr-[80px] hover:text-white transition-all duration-[1.5s] manboy'>Need A Personal Loan?</p>
+
+              <button className='bg-[rgb(12,51,121)] text-white py-[10px] px-[25px] whitespace-normal mt-[20px] mr-[150px] md:mr-[40px] lg:mr-[200px] rounded-[10px]'>
+                <Link to='/apply'>Apply Now</Link>
+              </button>
+            </div>
+
+           </div>
+
+        </div>
+      </div>
+      
+      <Loan2/>
 
     </div>
 
---
+++ client/src/LoanprogramComponents/Loan2.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+
+
+// const loanProgram = [
+//     {img: "https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc07.png"},
+//     {name: "Home Purchase", list: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+//     {name: "Refinancing", list: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+//     {name: "Conventional Loans", list: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+//     {name: "Remodeling Loans", list: "Lorem ipsum dolor sit amet consectetur adipisicing elit."},
+// ];
+
+function Loan2() {
+  return (
+    <div>
+      
+      <div className='bg-white h-[1440px] mt-[60px] lg:mt-[100px] md:h-[800px] lg:h-[750px] mb-[200px] grid grid-cols-1 md:grid-cols-2'>
+        
+        <div className='grid grid-cols-1 h-[800px] lg:h-[570px] bg-white'>
+            
+            <div className='bg-[rgb(248,249,251)] h-[180px] lg:h-[120px] w-[367px] md:w-[375px] lg:w-[630px] ml-[10px] lg:ml-[80px] grid grid-cols-[21%,79%] lg:grid-cols-[13%,87%] rounded-[10px]'>
+                <div>
+                     <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc07.png" className='h-[50px] ml-[20px] mt-[10px] cursor-pointer' />
+                </div>
+                <div>
+                    <p className='text-[rgb(38,106,225)] text-[20px] mt-[15px] cursor-pointer'>Home Purchase</p>
+                    <p className='text-[rgb(186,141,139)] text-[15px] mt-[10px] w-[200px] lg:w-[500px] tracking-wide cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipiscing, elit phasellus tincidunt magnis dictum magna cras mi ullamcorper himenaeos</p>
+                </div>
+            </div>
+
+            <div className='bg-[rgb(248,249,251)] h-[180px] lg:h-[120px] w-[367px] md:w-[375px] lg:w-[630px] ml-[10px] lg:ml-[80px] grid grid-cols-[21%,79%] lg:grid-cols-[13%,87%] rounded-[10px]'>
+                <div>
+                     <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc07.png" className='h-[50px] ml-[20px] mt-[10px] cursor-pointer' />
+                </div>
+                <div>
+                    <p className='text-[rgb(38,106,225)] text-[20px] mt-[15px] cursor-pointer'>Refinancing</p>
+                    <p className='text-[rgb(186,141,139)] text-[15px] mt-[10px] w-[200px] lg:w-[500px] tracking-wide cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipiscing, elit phasellus tincidunt magnis dictum magna cras mi ullamcorper himenaeos</p>
+                </div>
+            </div>
+
+            <div className='bg-[rgb(248,249,251)] h-[180px] lg:h-[120px] w-[367px] md:w-[375px] lg:w-[630px] ml-[10px] lg:ml-[80px] grid grid-cols-[21%,79%] lg:grid-cols-[13%,87%] rounded-[10px]'>
+                <div>
+                     <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc07.png" className='h-[50px] ml-[20px] mt-[10px] cursor-pointer' />
+                </div>
+                <div>
+                    <p className='text-[rgb(38,106,225)] text-[20px] mt-[15px] cursor-pointer'>Conventional Loans</p>
+                    <p className='text-[rgb(186,141,139)] text-[15px] mt-[10px] w-[200px] lg:w-[500px] tracking-wide cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipiscing, elit phasellus tincidunt magnis dictum magna cras mi ullamcorper himenaeos</p>
+                </div>
+            </div>
+
+            <div className='bg-[rgb(248,249,251)] h-[180px] lg:h-[120px] w-[367px] md:w-[375px] lg:w-[630px] ml-[10px] lg:ml-[80px] grid grid-cols-[21%,79%] lg:grid-cols-[13%,87%] rounded-[10px]'>
+                <div>
+                     <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/ameioc07.png" className='h-[50px] ml-[20px] mt-[10px] cursor-pointer' />
+                </div>
+                <div>
+                    <p className='text-[rgb(38,106,225)] text-[20px] mt-[15px] cursor-pointer'>Remodeling Loans</p>
+                    <p className='text-[rgb(186,141,139)] text-[15px] mt-[10px] w-[200px] lg:w-[500px] tracking-wide cursor-pointer'>Lorem ipsum dolor sit amet consectetur adipiscing, elit phasellus tincidunt magnis dictum magna cras mi ullamcorper himenaeos</p>
+                </div>
+            </div>
+           
+        </div>
+
+        
+        <div className='bg-white lg:h-[750px]'>
+            <img src="https://demo.strongtheme.com/americancredit/wp-content/uploads/sites/4/2021/06/amecreho031-536x1024.png" className='h-[640px] lg:h-[750px] lg:w-[400px] lg:mt-[0px] lg:ml-[120px] w-[330px] ml-[30px] md:mt-[160px]'/>
+        </div>
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default Loan2
